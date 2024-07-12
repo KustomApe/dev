3c5fbfe92b9e0f2b6d6bfdde5e8c55aabca56f6d
kustomApe <kustomape@gmail.com>
2024-07-12 17:18:33 +0900

    Removes all the app.:

--- ex6/message/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "message"
-
-urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('articleCreate/',views.ArticleCreate.as_view(),name = 'article_create'),
-]
