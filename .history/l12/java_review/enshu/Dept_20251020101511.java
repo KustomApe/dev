7b30e1c4931ac055ee4273ea794532d7f31e7b23
ask35 <ask35@ask35s-MacBook-Pro.local>
2025-10-29 09:45:51 +0900

    Adds several changes.

---
+++ .history/l12/java_review/enshu/Dept_20251020101511.java
@@ -0,0 +1,7 @@
+package enshu;
+
+public class Dept {
+    public static void main(String[], args) {
+        return Null;
+    }
+}
---
+++ .history/l12/java_review/enshu/Dept_20251020101608.java
@@ -0,0 +1,17 @@
+package enshu;
+
+public class Dept {
+    private int deptNo;
+
+    private String dname;
+
+    private String loc;
+
+    public int getDeptNo() {
+        return deptNo;
+    }
+
+    public static void main(String[], args) {
+        return Null;
+    }
+}
